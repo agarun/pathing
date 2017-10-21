b5ac9127319c82c84a62a68041ecba56a3c91463
agarun <19801205+agarun@users.noreply.github.com>
2017-10-21 02:08:22 -0400

    Reorder graphs.js class structure

--- js/graph.js
+++ js/graph.js
@@ -1,18 +1,24 @@
 class Graph {
   constructor() {
-
+    this.collection = []; // collection of nodes
   }
+}
 
-  node() {
+class Node {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.neighbors = []; // node's neighboring edges
+    this.discovered = false; // not fully visited yet b/c not part of MST TODO: color
   }
+}
 
-  edge(nodeFrom, nodeTo) {
+class Edge {
+  constructor(nodeFrom, nodeTo) {
     this.nodeFrom = nodeFrom;
     this.nodeTo = nodeTo;
     this.weight = Math.random(); // edges initialized with a random weight
   }
 }
 
-export default Graph;
+export { Graph, Node, Edge };
