7fc5832f8578807dfcb0a44ce5d7bcc15d3c7d9e
agarun <19801205+agarun@users.noreply.github.com>
2017-10-21 22:11:37 -0400

    Fix minimum spanning tree generation

--- js/generator.js
+++ js/generator.js
@@ -1,117 +1,140 @@
-import { Graph, Node, Edge } from './graph.js'
+import { Graph, Node, Edge } from './graph.js';
+import Draw from './draw.js';
+import * as CNS from './constants.js';
 
 class MazeGenerator {
-  constructor(canvas, cellSize) {
-    this.canvas = canvas;
-    this.ctx = canvas.getContext("2d");
-    this.cellSize = cellSize;
+  constructor(canvas) {
+    this.draw = new Draw(canvas, canvas.getContext('2d'));
 
-    // undirected graph
+    // undirected graph to which we will add random edge weights
     this.graph = new Graph();
 
     // the frontier represents the cells that surround or 'wall in' the maze path
     // i.e. the cells that aren't 'part' of the maze but are neighbors of current cell
     this.frontier = [];
 
-    // to observe a cell's surroundings, we store information about each of their neighbors
-    this.directions = {
-      NORTH: 0, //   0
-      SOUTH: 2, // 3 * 1
-      EAST: 1, //    2
-      WEST: 3,
-    };
-
     // minimum spanning tree (MST) -> all the edges of our edge-weighted, undirected
     // graph that connects all the vertices with the minimum possible total edge weight
-    this.tree = [];
+    // this is a subset of this.graph.collection
+    // represented by an adjacency list in an object, where the key is the node
+    // identifier & the value is an array of neighbors
+    this.tree = {};
   }
 
   grid() {
-    // each node will have a corresponding cell in the graph's collection array
-    const x = this.canvas.height / this.cellSize; // 'cols'
-    const y = this.canvas.width / this.cellSize; // 'rows'
-
     // initialize & build the maze grid points
     const graph = this.graph;
-    for (let i = 0; i < y; i += 1) {
-      graph.collection[i] = [];
-      for (let j = 0; j < x; j += 1) {
-        graph.collection[i][j] = new Node(j, i);
+    for (let r = 0; r < CNS.NUMROWS; r += 1) {
+      graph.collection[r] = [];
+      for (let c = 0; c < CNS.NUMCOLS; c += 1) {
+        graph.collection[r][c] = new Node(c, r);
       }
     }
 
-    // initialize each node's edges in each available direction
-    for (let i = 0; i < y; i += 1) {
-      for (let j = 0; j < x; j += 1) {
+    // the undirected graph's node edges are linked in each direction
+    for (let r = 0; r < CNS.NUMROWS; r += 1) {
+      for (let c = 0; c < CNS.NUMCOLS; c += 1) {
         // in randomized prim's, edges ('passages') have random weights
         // the `if` statements keep us within range. we generate weighted edges.
-        if (i !== y - 1) {
-          graph.collection[i][j].neighbors[this.directions.NORTH] =
-            new Edge(graph.collection[i + 1][j], graph.collection[i][j]);
-          graph.collection[i][j].neighbors[this.directions.SOUTH] =
-            new Edge(graph.collection[i][j], graph.collection[i + 1][j]);
+        if (r !== CNS.NUMROWS - 1) {
+          graph.collection[r][c].neighbors[CNS.DIRECTIONS.SOUTH] =
+            new Edge(graph.collection[r][c], graph.collection[r + 1][c]);
+          // undirected linkage (edge between N + S has 1 random weight):
+          graph.collection[r + 1][c].neighbors[CNS.DIRECTIONS.NORTH] =
+            new Edge(graph.collection[r + 1][c], graph.collection[r][c],
+              graph.collection[r][c].neighbors[CNS.DIRECTIONS.SOUTH].weight);
         }
-        if (j !== x - 1) {
-          graph.collection[i][j].neighbors[this.directions.EAST] =
-            new Edge(graph.collection[i][j], graph.collection[i][j + 1]);
-          graph.collection[i][j].neighbors[this.directions.WEST] =
-            new Edge(graph.collection[i][j + 1], graph.collection[i][j]);
+        if (c !== CNS.NUMCOLS - 1) {
+          graph.collection[r][c].neighbors[CNS.DIRECTIONS.EAST] =
+            new Edge(graph.collection[r][c], graph.collection[r][c + 1]);
+          // undirected linkage (edge between E + W has 1 random weight):
+          graph.collection[r][c + 1].neighbors[CNS.DIRECTIONS.WEST] =
+            new Edge(graph.collection[r][c + 1], graph.collection[r][c],
+              graph.collection[r][c].neighbors[CNS.DIRECTIONS.EAST].weight);
         }
       }
     }
-
-    return this.graph.collection;
   }
 
   // "Prim's algorithm is a greedy algorithm that finds a minimum spanning tree
   // for a weighted undirected graph" (Wikipedia). This is its randomized implementation.
   // It can be applied as a maze generation algorithm by building a maze "frontier"
   prims() {
-    const lastNode = this.tree[this.tree.length - 1]
-
     // the maze is built along the edge with the lowest weight
-    // that choice must not intersect a previously visited node
+    // that choice must not connect with a previously visited node
     // (1) observe the frontier to the current cell
     // (2) compare the edge weights & set the minimum cell
+    let newEdge = new Edge(null, null, 1); // dummy edge, no weight to compare against
     for (let i = 0; i < this.frontier.length; i += 1) {
-      // making sure it hasn't been visited before
+      if (this.frontier[i].nodeFrom.discovered && this.frontier[i].nodeTo.discovered) {
+        this.frontier.splice(i, 1);
+      } else if (this.frontier[i].weight < newEdge.weight && !this.frontier[i].nodeTo.discovered) {
+        newEdge = this.frontier[i];
+      }
     }
+    newEdge.nodeTo.discovered = true;
 
     // re-build the frontier at the new node for each of 4 directions
-    for (let j = 0; j < 4; j += 1) {
-      // doing some stuff, making sure it's a valid node
-    }
+    Object.values(CNS.DIRECTIONS).forEach((direction) => {
+      // there should be a cell at the direction && it should be undiscovered
+      if (
+        (newEdge.nodeTo.neighbors[direction] !== undefined)
+        && (!newEdge.nodeTo.neighbors[direction].nodeTo.discovered)
+      ) {
+        this.frontier.push(newEdge.nodeTo.neighbors[direction]);
+      }
+    });
 
-    // add the edge to a minimal spanning tree
-    this.tree.push(newEdge, newEdge.nodeTo);
-    
-    // add the other node of this edge to the set
-    // pick & add edges until the set includes every node in the graph
-    // color in the node since it was (1) visited and (2) parsed
+    // the computer will walk to `nodeTo` in some direction. add nodeTo as a new
+    // neighbor of nodeFrom. note nodeFrom can have multiple neighbors in a maze!
+    const currentPos = `${newEdge.nodeFrom.x}, ${newEdge.nodeFrom.y}`;
+
+    // each grid node shown by [edge, node] where edge is the passage to the next node
+    this.tree[currentPos] === undefined
+      ? this.tree[currentPos] = [[newEdge, newEdge.nodeTo]]
+      : this.tree[currentPos].push([newEdge, newEdge.nodeTo]);
+
+    // color in the node since it was (1) visited and (2) added, and
+    // color the edge from the previous node to nodeTo
+    // a gradient of 20 colors determines the fill color
+    let colorStep = (Object.keys(this.tree).length / CNS.PROGRESS);
+    colorStep = ((Math.ceil(colorStep * 20)) / 20).toFixed(2);
+    this.draw.drawEdge(newEdge, colorStep);
+    this.draw.drawNode(newEdge.nodeTo, colorStep);
   }
 
   build() {
     // we'll start building from the node at top-left corner.
     // our minimum spanning tree's root is this top-left node:
-    this.tree = this.graph.collection[0][0];
+    const firstNode = this.graph.collection[0][0];
+    this.tree[`${firstNode.x}, ${firstNode.y}`] = [[new Edge(firstNode, firstNode, 1), firstNode]];
+    this.draw.drawNode(firstNode);
+
+    this.graph.collection[0][0].discovered = true;
 
     // if we're in the top-left corner, we are walled in by 1 southern cell &
-    // 1 eastern cell. we can visit either:
-    this.frontier.push(this.graph.collection[0][0].neighbors[this.directions.SOUTH],
-                       this.graph.collection[0][0].neighbors[this.directions.EAST]);
+    // 1 eastern cell. we can visit either of them, depending on the rand weight
+    this.frontier.push(
+      this.graph.collection[0][0].neighbors[CNS.DIRECTIONS.SOUTH],
+      this.graph.collection[0][0].neighbors[CNS.DIRECTIONS.EAST],
+    );
 
     // TODO: implement customization options
+    // IDEA: d3.timer might offer better performance
     // repeat the prims algorithm until the graph is complete
     // uses an arbitrary time interval (ms) at which to build the maze
-    while (somecondition) {
-      const time = 50;
-      setInterval(() => this.prim(), time);
-    }
-  }
+    let progress = 0;
+    const time = 0;
+    const timer = setInterval(() => {
+      this.prims();
+      progress += 1;
+      if (progress === CNS.PROGRESS) { // every node in graph.collection is discovered
+        clearInterval(timer);
+      }
+    }, time);
 
-  draw(node) {
-    this.ctx.fillStyle = 'white'; // TODO: temp color
-    this.ctx.fillRect(node.x, node.y, this.cellSize, this.cellSize);
+    console.log(this.tree);
+    return this.tree;
   }
 }
 
