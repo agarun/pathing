557b8cfa8bc7b9b3fe139843b9f0ec28cda4a8ff
agarun <19801205+agarun@users.noreply.github.com>
2017-10-21 20:40:14 -0400

    Update constants module

--- js/constants.js
+++ js/constants.js
@@ -8,12 +8,28 @@ const BLOCKWIDTH = CELLSIZE * 2;
 const NUMROWS = HEIGHT / BLOCKWIDTH;
 const NUMCOLS = WIDTH / BLOCKWIDTH;
 
+// to observe a cell's surroundings, we store information about each of their neighbors
+const DIRECTIONS = {
+  NORTH: 0, //   0
+  SOUTH: 2, // 3 * 1
+  EAST: 1, //    2
+  WEST: 3,
+};
+
 // setInterval() terminates when each node of NUMROWS & NUMCOLS is traversed
 const PROGRESS = ((WIDTH * HEIGHT) / (BLOCKWIDTH ** 2)) - 1;
 
-// TODO: array of colors & choosing darker colors in steps
-// const PRIMSCOLOR = '#7af9da';
-const PRIMSCOLOR = '#54ff9e';
+const PRIMSCOLORS = {
+  0.00: "#ffffff", 0.05: '#ffffff', 0.10: '#f5fff9',
+  0.15: '#f5fff9', 0.20: '#f0fff6', 0.25: '#eafbf1',
+  0.30: '#e2fced', 0.35: '#d2fae3', 0.40: '#c7fcde',
+  0.45: '#aff6cd', 0.50: '#a0f4c4', 0.55: '#93f5bd',
+  0.60: '#7cf4b0', 0.65: '#67f6a5', 0.70: '#54ff9e',
+  0.75: '#49f694', 0.80: '#ffffff', 0.85: '#ffffff',
+  0.90: '#ffffff', 0.95: '#ffffff', 1.00: '#ffffff',
+};
+const VISITCOLOR = 'rgba(249, 63, 44, 0.8)';
+const SOLUTIONCOLOR = '#120b3c'; // webpage background
 
 export {
   WIDTH,
@@ -22,6 +38,9 @@ export {
   BLOCKWIDTH,
   NUMROWS,
   NUMCOLS,
+  DIRECTIONS,
   PROGRESS,
-  PRIMSCOLOR,
+  PRIMSCOLORS,
+  VISITCOLOR,
+  SOLUTIONCOLOR,
 };
