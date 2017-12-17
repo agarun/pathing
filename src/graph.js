4e9f81425126384e1efd02e008750251c0356bdc
agarun <19801205+agarun@users.noreply.github.com>
2017-12-17 11:21:55 -0500

    Add public Graph class method to reconstruct solution paths

--- src/graph.js
+++ src/graph.js
@@ -4,15 +4,27 @@ class Graph {
   constructor() {
     this.collection = []; // collection of nodes
   }
+
+  static reconstructPath(source, target, pathData, draw) {
+    let predecessor = target;
+    while (predecessor !== source) {
+      draw.drawPath(pathData[predecessor], 'solution', true);
+      const [[leadingEdge, previousNode]] = pathData[predecessor];
+      predecessor = previousNode;
+    }
+
+    this.searching = 0;
+    draw.drawEnds([source, target]);
+  }
 }
 
 class Node {
   constructor(x, y) {
     this.x = x * CNS.BLOCKWIDTH;
     this.y = y * CNS.BLOCKWIDTH;
-    this.neighbors = []; // node's neighboring edges
+    this.neighbors = []; // up to four neighboring edges
     this.visited = false;
-    this.discovered = false; // true when added to MST
+    this.discovered = false; // true if present in Prim's min spanning tree
   }
 }
 
@@ -20,7 +32,7 @@ class Edge {
   constructor(nodeFrom, nodeTo, weight = Math.random()) {
     this.nodeFrom = nodeFrom;
     this.nodeTo = nodeTo;
-    this.weight = weight; // edges initialized with a random weight
+    this.weight = weight; // 0 <= random weight < 1
   }
 }
 
