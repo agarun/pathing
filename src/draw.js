94326e709df35a57285e2cbdface4da796e7dcfd
agarun <19801205+agarun@users.noreply.github.com>
2017-12-16 23:09:25 -0500

    Update canvas draw methods

--- src/draw.js
+++ src/draw.js
@@ -7,6 +7,10 @@ class Draw {
     this.drawn = {};
   }
 
+  resetState() {
+    this.drawn = {};
+  }
+
   drawEdge(edge, colorStep, customColor) {
     this.ctx.fillStyle = customColor || CNS.PRIMSCOLORS[colorStep];
     this.ctx.fillRect(
@@ -26,7 +30,8 @@ class Draw {
     this.ctx.fillStyle = style === 'visit' ? CNS.VISITCOLOR : CNS.SOLUTIONCOLOR;
 
     nodes.forEach((node) => {
-      const edgeId = `${node[0].nodeFrom.x} ${node[0].nodeFrom.y} ${node[0].nodeTo.x} ${node[0].nodeTo.y}`;
+      const edgeId = `${node[0].nodeFrom.x} ${node[0].nodeFrom.y} ` +
+                     `${node[0].nodeTo.x} ${node[0].nodeTo.y}`;
       if (!this.drawn[edgeId] || force) {
         this.drawEdge(node[0]);
         this.drawn[edgeId] = true;
@@ -40,22 +45,24 @@ class Draw {
     });
   }
 
-  // TODO: DRY
   drawEnds(nodes) {
     const startAndEnd = nodes.map(node => node.split(', '));
+    const cellSize = [CNS.CELLSIZE * 1.5, CNS.CELLSIZE * 1.5];
+    const cellDelta = CNS.CELLSIZE / 4;
+
     this.ctx.strokeStyle = CNS.SOLUTIONCOLOR;
     this.ctx.lineWidth = 4;
     this.ctx.fillStyle = CNS.STARTCOLOR;
-    this.ctx.fillRect(startAndEnd[0][0], startAndEnd[0][1], (CNS.CELLSIZE * 1.5), (CNS.CELLSIZE * 1.5));
-    this.ctx.strokeRect(startAndEnd[0][0] - (CNS.CELLSIZE / 4), startAndEnd[0][1] - (CNS.CELLSIZE / 4), (CNS.CELLSIZE * 1.5), (CNS.CELLSIZE * 1.5));
+    this.ctx.fillRect(startAndEnd[0][0], startAndEnd[0][1], ...cellSize);
+    this.ctx.strokeRect(startAndEnd[0][0] - cellDelta, startAndEnd[0][1] - cellDelta, ...cellSize);
     this.ctx.fillStyle = CNS.ENDCOLOR;
-    this.ctx.fillRect(startAndEnd[1][0], startAndEnd[1][1], (CNS.CELLSIZE * 1.5), (CNS.CELLSIZE * 1.5));
-    this.ctx.strokeRect(startAndEnd[1][0] - (CNS.CELLSIZE / 4), startAndEnd[1][1] - (CNS.CELLSIZE / 4), (CNS.CELLSIZE * 1.5), (CNS.CELLSIZE * 1.5));
+    this.ctx.fillRect(startAndEnd[1][0], startAndEnd[1][1], ...cellSize);
+    this.ctx.strokeRect(startAndEnd[1][0] - cellDelta, startAndEnd[1][1] - cellDelta, ...cellSize);
     this.ctx.stroke();
   }
 
   static destructurePosition(nodePosition) {
-    if (typeof nodePosition === 'string' || nodePosition instanceof String) {
+    if (typeof nodePosition === 'string') {
       return nodePosition.split(', ');
     }
     return [nodePosition.x, nodePosition.y];
