503f3d5553b3fa9907b8c0d48cb2e5400b251886
agarun <19801205+agarun@users.noreply.github.com>
2017-12-17 16:41:34 -0500

    Add function to view old completed searches

--- src/dom.js
+++ src/dom.js
@@ -20,6 +20,26 @@ const DOMHelper = {
       document.getElementById(id).classList.add('search-btn');
     });
   },
+  addPreviousSearch(event) {
+    const canvasHistory = document.getElementById('canvas-history');
+    const historyCtx = canvasHistory.getContext('2d');
+    const newHistory = document.createElement('button');
+    const finalSearchImage = document
+      .getElementById('canvas')
+      .getContext('2d')
+      .getImageData(0, 0, CNS.WIDTH, CNS.HEIGHT);
+
+    newHistory.addEventListener('mouseover', () => {
+      historyCtx.putImageData(finalSearchImage, 0, 0);
+    });
+
+    newHistory.addEventListener('mouseout', () => {
+      historyCtx.clearRect(0, 0, canvasHistory.width, canvasHistory.height);
+    });
+
+    newHistory.textContent = event.target.id;
+    document.getElementById('past-searches').appendChild(newHistory);
+  },
 };
 
 export default DOMHelper;
