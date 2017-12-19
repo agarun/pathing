7018b2fcdfbf691f8f2d152cb482c74ef93d7991
agarun <19801205+agarun@users.noreply.github.com>
2017-12-18 23:06:25 -0500

    Fix DOM helper method

--- src/dom.js
+++ src/dom.js
@@ -20,7 +20,7 @@ const DOMHelper = {
       document.getElementById(id).classList.add('search-btn');
     });
   },
-  addPreviousSearch(event) {
+  addPreviousSearch(search) {
     const canvasHistory = document.getElementById('canvas-history');
     const historyCtx = canvasHistory.getContext('2d');
     const newHistory = document.createElement('button');
@@ -29,15 +29,19 @@ const DOMHelper = {
       .getContext('2d')
       .getImageData(0, 0, CNS.WIDTH, CNS.HEIGHT);
 
+    canvasHistory.width = CNS.WIDTH;
+    canvasHistory.height = CNS.HEIGHT;
+
     newHistory.addEventListener('mouseover', () => {
       historyCtx.putImageData(finalSearchImage, 0, 0);
     });
 
     newHistory.addEventListener('mouseout', () => {
-      historyCtx.clearRect(0, 0, canvasHistory.width, canvasHistory.height);
+      historyCtx.clearRect(0, 0, CNS.WIDTH, CNS.HEIGHT);
     });
 
-    newHistory.textContent = event.target.id;
+    newHistory.textContent = search.id;
+    newHistory.classList.add('past-search-btn');
     document.getElementById('past-searches').appendChild(newHistory);
   },
 };
